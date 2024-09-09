a1674d7814b87514cbad150485290b97bffcb825
Filippo Bigi <98903385+frostedoyster@users.noreply.github.com>
2024-09-09 15:27:57 +0200

    Update to `sphericart` 0.5.0 (#1)
    * Update to sphericart 0.5.0
    * Add `SolidHarmonics`

--- spex/angular/solid_harmonics.py
+++ spex/angular/solid_harmonics.py
@@ -1,21 +1,22 @@
 from torch.nn import Module
 
-from sphericart.torch import SphericalHarmonics as Sph
+from sphericart.torch import SolidHarmonics as Soh
 
 from spex.engine import Specable
 
 
-class SphericalHarmonics(Module, Specable):
-    """Spherical harmonics.
+class SolidHarmonics(Module, Specable):
+    """Solid harmonics.
 
-    Expands vectors in spherical harmonics using ``sphericart``.
+    Expands vectors in solid harmonics as defined in ``sphericart``.
     See https://sphericart.readthedocs.io/en/latest/maths.html;
     this computes the Wikipedia definition of real spherical harmonics,
-    which are ortho-normalised when integrated over the sphere.
+    which are ortho-normalised when integrated over the sphere, times
+    ``r**l``.
 
     Inputs are expexted to be a ``[pair, 3]``, i.e. 2D ``Tensor`` of vectors,
     and are returned as ``[pair, sum([2*l+1 for i in range(max_angular+1)])]``,
-    i.e. all spherical harmonics flattened across the second dimension. We use the
+    i.e. all solid harmonics flattened across the second dimension. We use the
     order ``(l=0 m=0) (l=1 m=-1) (l=1 m=0) (l=1 m=+1) (l=2, m=-2) ...``. (Recall
     that each ``m`` runs from ``-l`` to ``+l`` in steps of ``1``.)
 
@@ -23,16 +24,16 @@ class SphericalHarmonics(Module, Specable):
     can therefore be used in ``torch.split`` to obtain separate features per ``l``.
 
     Attributes:
-        max_angular (int): The maximum spherical harmonic order ``l`` to compute.
+        max_angular (int): The maximum solid harmonic order ``l`` to compute.
         m_per_l (list): Number of ``m`` for each ``l``.
 
     """
 
     def __init__(self, max_angular):
-        """Initialise SphericalHarmonics.
+        """Initialise SolidHarmonics.
 
         Args:
-            max_angular (int): The maximum spherical harmonic order ``l`` to compute.
+            max_angular (int): The maximum solid harmonic order ``l`` to compute.
         """
         super().__init__()
 
@@ -40,21 +41,21 @@ class SphericalHarmonics(Module, Specable):
 
         self.m_per_l = [2 * l + 1 for l in range(max_angular + 1)]
 
-        self.sph = Sph(l_max=self.max_angular)
+        self.soh = Soh(l_max=self.max_angular)
 
         self.spec = {"max_angular": self.max_angular}
 
     def forward(self, R):
-        """Compute spherical harmonics for input vectors.
+        """Compute solid harmonics for input vectors.
 
         Args:
             R (Tensor): Input vectors of shape ``[pair, 3]``.
 
         Returns:
-            Spherical harmonics of shape
+            Solid harmonics of shape
                 ``[pair, sum([2*l+1 for i in range(max_angular+1)])]``.
 
         """
         # R: [pair, 3 (x,y,z)]
         # todo: make special case for "mps" backend (need to move to `cpu`)
-        return self.sph.compute(R)  # -> [pair, (l=0 m=0) (l=1 m=-1) (l=1 m=0) ...]
+        return self.soh.compute(R)  # -> [pair, (l=0 m=0) (l=1 m=-1) (l=1 m=0) ...]
